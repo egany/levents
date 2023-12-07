962ee27bd59dec3e4a9c5abce6beefbfdaa00da1
nnthuanegany <thuan.nguyen@egany.com>
2023-12-07 20:37:52 +0700

    Update email template for OTP

--- core/send-email-otp.js
+++ core/send-email-otp.js
@@ -6,12 +6,10 @@ const sgMail = require("@sendgrid/mail");
  */
 async function sendEmailOTP(params) {
   sgMail.setApiKey(process.appSettings.sendgridApiKey);
-  sgMail.setApiKey(SENDGRID_API_KEY);
   let text = `${params.OTP} là mã xác thực (OTP) của bạn tại website levents.asia. Vui lòng không chia sẻ mã xác thực dưới bất kỳ hình thức nào. Xin cảm ơn và chúc bạn mua sắm vui vẻ!`
   const msg = {
     to: params.email,
     from: process.appSettings.sendgridMailer, // Use the email address or domain you verified above
-    from: SENDGRID_MAILER, // Use the email address or domain you verified above
     subject: `${params.OTP} là mã xác thực (OTP) của bạn`,
     text: `${text}`,
     html: `<div>
